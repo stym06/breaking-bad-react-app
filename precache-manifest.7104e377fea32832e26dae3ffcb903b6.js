70aa9e685bb114c8942f9586fb9c4dc52a76e27a
Satyam Raj <satyam.raj@olacabs.com>
2020-10-10 18:19:48 +0530

    Updates

---
+++ precache-manifest.7104e377fea32832e26dae3ffcb903b6.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8a3a4e726f8f24037f698597e233b4e",
+    "url": "/breaking-bad-react-app/index.html"
+  },
+  {
+    "revision": "3a14654c286dc58003f3",
+    "url": "/breaking-bad-react-app/static/css/main.fd36e1f6.chunk.css"
+  },
+  {
+    "revision": "afcd59db69b554cc074b",
+    "url": "/breaking-bad-react-app/static/js/2.b60bedf1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/breaking-bad-react-app/static/js/2.b60bedf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a14654c286dc58003f3",
+    "url": "/breaking-bad-react-app/static/js/main.63e2e91f.chunk.js"
+  },
+  {
+    "revision": "95aecc04baba64494f32",
+    "url": "/breaking-bad-react-app/static/js/runtime-main.5e7aca39.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/breaking-bad-react-app/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/breaking-bad-react-app/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/breaking-bad-react-app/static/media/spinner.235b9b3e.gif"
+  }
+]);
